31e8f9551e617f8de45a2f69f3c974455d38ffe1
yernarM <yernar.makhutov@mail.ru>
2022-09-30 09:51:24 +0500

    first commit

---
+++ dist/script.js
@@ -0,0 +1,9 @@
+const virar = new Audio('https://github.com/Moonlight404/Portfolio/blob/master/virar.mp3?raw=true')
+
+$('.page').click(function() {
+		virar.play()
+    $(this).removeClass('no-anim').toggleClass('flipped');
+   $('.page > div').click(function(e) {
+        e.stopPropagation();
+   });
+});
